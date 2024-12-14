ef54bce6b9815b3a2d87327923cbd57cc0835236
Nikita <nikita2007ua@gmail.com>
2024-12-14 14:00:51 +0300

    +http

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Главная</h1><div><a href='/home'>Домой</a></div>")
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<b>Сервер работает</b>")
+}
+
+func control() {
+	fmt.Println("Запуск сервера")
+	//Урлы страниц
+	http.HandleFunc("/", index)
+	http.HandleFunc("/home/", home)
+
+	//Слушатель порта
+	http.ListenAndServe("localhost:8888", nil)
+}
+
 func main() {
-	println("Hello World!")
+	control()
 }
